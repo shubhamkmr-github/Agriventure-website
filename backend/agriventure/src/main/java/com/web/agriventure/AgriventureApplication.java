f9695cab84a83c01f609bb917639ce8ed136415c
shubham kumar <shubhamkmrbhardwaj@gmail.com>
2025-04-03 18:58:35 +0530

    project creation

---
+++ backend/agriventure/src/main/java/com/web/agriventure/AgriventureApplication.java
@@ -0,0 +1,13 @@
+package com.web.agriventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriventureApplication.class, args);
+	}
+
+}
